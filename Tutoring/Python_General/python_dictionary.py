5f08c6b78d91d6e587cbc831c1a9f0d6040c419d
JoenTheWizard <75371093+JoenTheWizard@users.noreply.github.com>
2023-04-01 10:29:58 +1100

    Create python_dictionary.py

---
+++ Tutoring/Python_General/python_dictionary.py
@@ -0,0 +1,11 @@
+#I will give you a file later, it will contain a large list of names and values.
+#Here is an example of what a line of this file would look like
+
+#Bob : 20, 30, 10, 4 (Some of the scores list may vary in length)
+#So Bob should be the key. And its corresponding values should be { mean: 16 }
+#So the resulting value should be { 'Bob' : { mean : 16 } }
+
+#Optional: 
+#At the end of the program it should return the person with the highest 'mean' value
+#If for example 'Bob' had the highest mean, at the end print this
+#"Bob has the highest mean value"
