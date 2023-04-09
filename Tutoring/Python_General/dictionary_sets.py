8b6efa639f2aefe2b193dd2bf87a4f8e76743beb
JoenTheWizard <75371093+JoenTheWizard@users.noreply.github.com>
2023-04-09 11:16:48 +1000

    Create dictionary_sets.py

---
+++ Tutoring/Python_General/dictionary_sets.py
@@ -0,0 +1,18 @@
+#We need to take two dictionaries in this function 'common_keys'
+#This function should return a set
+
+#Find the intersection of these two dictionaries and return the set
+
+#For this question you should want the intersection of the key values from the dictionary
+#The example output should look like this:
+#{"banana", "orange"}
+
+def common_keys(dict1: dict, dict2: dict) -> set:
+    pass #Implement code
+
+#Use these dictionaries
+dict1 = {"apple": 3, "banana": 2, "orange": 1}
+dict2 = {"banana": 1, "orange": 4, "pear": 2}
+
+#Output here
+#print(common_keys(dict1, dict2))
