7d0337f53003fbf6d60d12059a67dcf9960f688f
JoenTheWizard <75371093+JoenTheWizard@users.noreply.github.com>
2023-04-16 11:06:07 +1000

    Create class_dicts.py

---
+++ Tutoring/Python_General/class_dicts.py
@@ -0,0 +1,17 @@
+# Create a class named Person that takes in a dictionary in the constructor 
+# for the same of this a dictionary has already been provided to you named 'people'
+
+# In this Person class, I want you to define a method called 'personInfo()' which reads the
+# values in the people dictionary and print their information
+
+#Define class here
+
+people = {
+    "Alice": {"name": "Alice", "age": 30, "city": "New York"},
+    "Bob": {"name": "Bob", "age": 25, "city": "San Francisco"},
+    "Charlie": {"name": "Charlie", "age": 35, "city": "Chicago"}
+}
+
+# Use this for printing/calling the class method
+#persons = Person(people)
+#persons.personInfo()
