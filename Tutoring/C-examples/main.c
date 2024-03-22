d3205b187f78f8feb5d241ab93ee33be44a26e90
JoenTheWizard <75371093+JoenTheWizard@users.noreply.github.com>
2024-03-23 01:42:59 +1100

    Update main.c

--- Tutoring/C-examples/main.c
+++ Tutoring/C-examples/main.c
@@ -5,7 +5,6 @@
 
 //If you want an optional and extra read on good 'style' for C programming here is:
 //https://felipec.github.io/good-taste/parts/1.html (https://github.com/mkirchner/linked-list-good-taste)
-//https://felipec.wordpress.com/2024/03/03/c-skill-issue-how-the-white-house-is-wrong/
 
 //Declare main (which returns int)
 int main() {
